38ef95de8bd754b3f561de3f0d95687d44dc9103
unknown <DoAnhTuan>
2019-01-15 14:26:30 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from models.character import Charater
+from models.user import User
 import mlab
+
 mlab.connect()
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "very secret"
 
 # Character.objects() lay het du lieu ra
 
@@ -21,10 +24,16 @@ def add_character():
      new_character.save()
     
      return "Gotcha"
+
 @app.route("/characters")
 def character():
-    character_list = Charater.objects()
-    return render_template("characters.html",c_list = character_list)
+     if "token" in session:
+      character_list = Charater.objects()
+      return render_template("characters.html",c_list = character_list)
+     else:
+          return redirect("/login?next=/characters")
+
+
 @app.route("/character/<given_id>")
 def character_detail(given_id):
     # #1. Get one character, dua vao id   
@@ -36,6 +45,51 @@ def character_detail(given_id):
      return "Not Found"
     else:
      return render_template("character_detail.html",character = character)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+     if request.method == "GET":
+          return render_template("login_form.html")
+     elif request.method == "POST":
+          form = request.form
+          username = form["username"]
+          password = form["password"]
+          found_user = User.objects(username=username).first()
+          if found_user is None:
+               return "User Not found"
+          elif found_user.password != password:
+               return "Invalid password"
+          else:
+               session["token"] = username
+               next = request.args.get("next")
+               if next is None or next == "":
+                 return "Logged in succesfully"
+               else:
+                    return redirect(next)
+
+
+     @app.route("/logout")
+     def logout():
+          del session["token"]
+          return redirect("/login")
+          if username == "admin" and password == "admin":
+            session["token"] = "admin"
+            return "Logged in succesfully"
+          else:
+            return "Failed to login"
+     return render_template("login_form.html")
+
+
+@app.route('/posts')
+def posts():
+     if "token" not in session:
+          return redirect("/login?next=/posts")
+     else:
+          username = session["token"]
+
+
+
+
 
 if __name__ == "__main__":
   app.run( debug=True)
